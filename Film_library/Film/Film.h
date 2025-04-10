fb2a3b74a692570a5417f30e4b36147a60b511d4
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-04-10 20:38:28 +0300

    code refactoring

--- Film_library/Film/Film.h
+++ Film_library/Film/Film.h
@@ -7,6 +7,6 @@ class Film {
     char* _film_genre;
     char* _country_of_issue;
     char* _year_of_issue;
-    int _average_estimation;  
+    int _average_estimation;
 };
 #endif  // FILM_LIBRARY_FILM_FILM_H_
