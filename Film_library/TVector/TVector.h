ce3ca92c1edcb2e771a6cea15b35a6f0ff15e08c
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-05-03 21:49:25 +0300

    added implementations of constructors, getters, some operators, and private methods for working with memory

--- Film_library/TVector/TVector.h
+++ Film_library/TVector/TVector.h
@@ -1,4 +1,7 @@
 // Copyright 2025 Maslennikova Mary
+#include <cstdlib>
+#include <stdexcept>
+#include <initializer_list>
 #ifndef FILM_LIBRARY_TVECTOR_TVECTOR_H_
 #define FILM_LIBRARY_TVECTOR_TVECTOR_H_
 
@@ -75,4 +78,175 @@ private:
     size_t recalculate_the_position(size_t index) const noexcept;
     size_t recalculate_the_address(size_t index) const noexcept;
 };
+
+template <class T>
+TVector<T>::TVector() : _data(nullptr), _size(0),
+_capacity(STEP_OF_CAPACITY), _deleted(0), _states(nullptr) {
+    set_memory(0);
+}
+template <class T>
+TVector<T>::TVector(size_t size, const T* data) : _data(nullptr), _size(size),
+_capacity(STEP_OF_CAPACITY), _deleted(0), _states(nullptr) {
+    set_memory(size);
+    if (data != nullptr) {
+        for (size_t i = 0; i < _size; i++) {
+            _data[i] = data[i];
+            _states[i] = busy;
+        }
+    }
+}
+template <class T>
+TVector<T>::TVector(size_t size, std::initializer_list<T> data) {
+    set_memory(size);
+    _deleted = 0;
+    size_t index = 0;
+    auto it = data.begin();
+    for (index; it != data.end(); index++, it++) {
+        _data[index] = *it;
+        _states[index] = busy;
+    }
+    while (index < _size) {
+        _data[index++] = 0;
+    }
+}
+template <class T>
+TVector<T>::TVector(std::initializer_list<T> data) {
+    size_t size = data.size();
+    set_memory(size);
+    _deleted = 0;
+    size_t index = 0;
+    auto it = data.begin();
+    for (index; it != data.end(); index++, it++) {
+        _data[index] = *it;
+        _states[index] = busy;
+    }
+    while (index < _size) {
+        _data[index++] = 0;
+    }
+}
+template <class T>
+TVector<T>::TVector(const TVector<T>& other) {
+    if (&other == NULL)
+        throw std::invalid_argument("The object was not received!\n");
+    _size = other._size;
+    _capacity = other._capacity;
+    _deleted = other._deleted;
+    set_memory(_size);
+    for (size_t i = 0; i < _size; i++) {
+        _data[i] = other._data[i];
+        _states[i] = other._states[i];
+    }
+}
+template <class T>
+TVector<T>::~TVector() {
+    delete[] _data;
+    delete[] _states;
+}
+
+template <class T>
+inline T* TVector<T>::data() noexcept {
+    return _data;
+}
+template <class T>
+inline size_t TVector<T>::size() const noexcept {
+    return _size;
+}
+template <class T>
+inline size_t TVector<T>::capacity() const noexcept {
+    return _capacity;
+}
+template <class T>
+inline const T* TVector<T>::data() const noexcept {
+    if (is_empty())
+        return nullptr;
+    T* new_address = recalculate_the_address(0);
+    return new_address;
+}
+template <class T>
+inline const T& TVector<T>::front() {
+    if (is_empty())
+        throw std::invalid_argument
+        ("There is no first element in the empty vector!\n");
+    size_t new_index = recalculate_the_position(0);
+    return _data[new_index];
+}
+template <class T>
+inline const T& TVector<T>::back() {
+    if (is_empty())
+        throw std::invalid_argument
+        ("There is no last element in the empty vector!\n");
+    size_t new_index = recalculate_the_position(_size - 1);
+    return _data[new_index];
+}
+template <class T>
+inline const T* TVector<T>::begin() const noexcept {
+    if (is_empty())
+        return nullptr;
+    T* new_address = recalculate_the_address(0);
+    return new_address;
+}
+template <class T>
+inline const T* TVector<T>::end() const noexcept {
+    if (is_empty())
+        return nullptr;
+    T* new_address = recalculate_the_address(_size - 1) + 1;
+    return new_address;
+}
+
+template <class T>
+const T& TVector<T>::operator[](size_t index) const {
+    if (index >= _size + _deleted) {
+        throw std::invalid_argument("The index goes beyond the boundaries\n");
+    }
+    size_t new_index = recalculate_the_position(index);
+    return _data[new_index];
+}
+template <class T>
+T& TVector<T>::operator[](size_t index) {
+    if (index >= _size + _deleted) {
+        throw std::invalid_argument("The index goes beyond the boundaries\n");
+    }
+    size_t new_index = recalculate_the_position(index);
+    return _data[new_index];
+}
+
+template <class T>
+inline bool TVector<T>::is_empty() const noexcept {
+    return _size == 0;
+}
+
+template <class T>
+void TVector<T>::set_memory(size_t size) noexcept {
+    _size = size;
+    _capacity = (_size / STEP_OF_CAPACITY + 1) * STEP_OF_CAPACITY;
+    delete[] _data;
+    delete[] _states;
+    _data = new T[_capacity];
+    _states = new State[_capacity];
+    for (size_t i = 0; i < _capacity; ++i) {
+        _states[i] = empty;
+    }
+}
+template <class T>
+size_t TVector<T>::recalculate_the_position(size_t index) const noexcept {
+    size_t count_busy = 0;
+    for (size_t i = 0; i < _size + _deleted; i++) {
+        if (_states[i] == busy) {
+            if (index == count_busy)
+                return i;
+            count_busy++;
+        }
+    }
+}
+template <class T>
+size_t TVector<T>::recalculate_the_address(size_t index) const noexcept {
+    size_t count_busy = 0;
+    for (size_t i = 0; i < _size + _deleted; i++) {
+        if (_states[i] == busy) {
+            if (index == count_busy)
+                return &_data[i];
+            count_busy++;
+        }
+    }
+}
 #endif  // FILM_LIBRARY_TVECTOR_TVECTOR_H_
