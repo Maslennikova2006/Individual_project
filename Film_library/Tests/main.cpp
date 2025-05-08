0dad5de244d330f14e1525e821dfc478e5c5e581
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-05-08 22:31:50 +0300

    code refactoring

--- Film_library/Tests/main.cpp
+++ Film_library/Tests/main.cpp
@@ -21,79 +21,78 @@ void set_color(int text_color, int bg_color) {
 }
 
 namespace TestSystem {
-    int count_success = 0, count_failed = 0;
-    void start_test(bool(*test)(), const char* name_of_test) {
+int count_success = 0, count_failed = 0;
+void start_test(bool(*test)(), const char* name_of_test) {
+    set_color(2, 0);
+    std::cout << "[ RUN      ] ";
+    set_color(7, 0);
+    std::cout << name_of_test << std::endl;
+
+    bool status = test();
+
+    if (status == true) {
         set_color(2, 0);
-        std::cout << "[ RUN      ] ";
-        set_color(7, 0);
-        std::cout << name_of_test << std::endl;
-
-        bool status = test();
-
-        if (status == true) {
-            set_color(2, 0);
-            std::cout << "[       OK ]" << std::endl;
-            count_success++;
-        }
-        else {
-            set_color(4, 0);
-            std::cout << "[  FAILED  ]" << std::endl;
-            count_failed++;
-        }
-        set_color(7, 0);
+        std::cout << "[       OK ]" << std::endl;
+        count_success++;
+    } else {
+        set_color(4, 0);
+        std::cout << "[  FAILED  ]" << std::endl;
+        count_failed++;
     }
+    set_color(7, 0);
+}
 
-    template <class T>
-    bool check(const T& expected, const T& actual) {
-        if (expected == actual) {
-            return true;
-        }
-        else {
-            std::cerr << "Expected result is " << expected << ", but actual is "
-                << actual << "." << std::endl;
-            return false;
-        }
+template <class T>
+bool check(const T& expected, const T& actual) {
+    if (expected == actual) {
+        return true;
+    } else {
+        std::cerr << "Expected result is " << expected << ", but actual is "
+            << actual << "." << std::endl;
+        return false;
     }
+}
 
-    void print_init_info() {
-        set_color(2, 0);
-        std::cout << "[==========] " << std::endl;
-        set_color(7, 0);
-    }
+void print_init_info() {
+    set_color(2, 0);
+    std::cout << "[==========] " << std::endl;
+    set_color(7, 0);
+}
 
-    void print_final_info() {
-        set_color(2, 0);
-        std::cout << "[==========] ";
+void print_final_info() {
+    set_color(2, 0);
+    std::cout << "[==========] ";
+    set_color(7, 0);
+    std::cout << count_success + count_failed << " test"
+        << (count_success + count_failed > 1 ? "s" : "")
+        << " ran." << std::endl;
+    set_color(2, 0);
+    std::cout << "[  PASSED  ] ";
+    set_color(7, 0);
+    std::cout << count_success << " test"
+        << (count_success > 1 ? "s" : "") << std::endl;
+    if (count_failed > 0) {
+        set_color(4, 0);
+        std::cout << "[  FAILED  ] ";
         set_color(7, 0);
-        std::cout << count_success + count_failed << " test"
-            << (count_success + count_failed > 1 ? "s" : "")
-            << " ran." << std::endl;
-        set_color(2, 0);
-        std::cout << "[  PASSED  ] ";
-        set_color(7, 0);
-        std::cout << count_success << " test"
-            << (count_success > 1 ? "s" : "") << std::endl;
-        if (count_failed > 0) {
-            set_color(4, 0);
-            std::cout << "[  FAILED  ] ";
-            set_color(7, 0);
-            std::cout << count_failed << " test"
-                << (count_failed > 1 ? "s." : ".") << std::endl;
-        }
+        std::cout << count_failed << " test"
+            << (count_failed > 1 ? "s." : ".") << std::endl;
     }
+}
 };  // namespace TestSystem
 
 #ifdef FIO_TESTS
 bool FIO_test_1_check_the_default_constructor() {
     FIO person;
     return TestSystem::check((std::string)"Ivan", person.get_first_name()) &&
-        TestSystem::check((std::string)"Ivanovich", person.get_second_name()) &&
-        TestSystem::check((std::string)"Ivanov", person.get_last_name());
+        TestSystem::check((std::string)"Ivanovich", person.get_second_name())
+        && TestSystem::check((std::string)"Ivanov", person.get_last_name());
 }
 bool FIO_test_2_check_the_initialization_constructor() {
     FIO person("Mary", "Vyacheslavovna", "Maslennikova");
     return TestSystem::check((std::string)"Mary", person.get_first_name()) &&
-        TestSystem::check((std::string)"Vyacheslavovna", person.get_second_name()) &&
+        TestSystem::check((std::string)"Vyacheslavovna",
+            person.get_second_name()) &&
         TestSystem::check((std::string)"Maslennikova", person.get_last_name());
 }
 bool FIO_test_3_throw_when_try_copy_vector() {
@@ -112,7 +111,8 @@ bool FIO_test_4_check_the_copy_constructor() {
     FIO person1("Anna", "Dmitrievna", "Danilova");
     FIO person2(person1);
     return TestSystem::check((std::string)"Anna", person2.get_first_name()) &&
-        TestSystem::check((std::string)"Dmitrievna", person2.get_second_name()) &&
+        TestSystem::check((std::string)"Dmitrievna",
+            person2.get_second_name()) &&
         TestSystem::check((std::string)"Danilova", person2.get_last_name());
 }
 bool FIO_test_5_check_the_setters() {
@@ -121,8 +121,8 @@ bool FIO_test_5_check_the_setters() {
     person.set_second_name("Andreevna");
     person.set_last_name("Osipova");
     return TestSystem::check((std::string)"Elena", person.get_first_name()) &&
-        TestSystem::check((std::string)"Andreevna", person.get_second_name()) &&
-        TestSystem::check((std::string)"Osipova", person.get_last_name());
+        TestSystem::check((std::string)"Andreevna", person.get_second_name())
+        && TestSystem::check((std::string)"Osipova", person.get_last_name());
 }
 bool FIO_test_6_check_the_exception_in_the_setters() {
     FIO person;
@@ -157,8 +157,8 @@ bool FIO_test_7_check_string_conversion_in_setters() {
     person.set_second_name("anDreEvnA");
     person.set_last_name("OsIpOvA");
     return TestSystem::check((std::string)"Elena", person.get_first_name()) &&
-        TestSystem::check((std::string)"Andreevna", person.get_second_name()) &&
-        TestSystem::check((std::string)"Osipova", person.get_last_name());
+        TestSystem::check((std::string)"Andreevna", person.get_second_name())
+        && TestSystem::check((std::string)"Osipova", person.get_last_name());
 }
 #endif
 int main() {
