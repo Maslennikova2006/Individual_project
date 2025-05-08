d4cc4f519cdee00658a67bb4b5a6ccf9d13b5007
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-05-08 20:36:51 +0300

    header changed

--- Film_library/FIO/FIO.h
+++ Film_library/FIO/FIO.h
@@ -1,11 +1,32 @@
 // Copyright 2025 Maslennikova Mary
 
+#include "string"
+
 #ifndef FILM_LIBRARY_FIO_FIO_H_
 #define FILM_LIBRARY_FIO_FIO_H_
 
 class FIO {
-    char* _first_name;
-    char* _second_name;
-    char* _last_name;
+    std::string _first_name;
+    std::string _second_name;
+    std::string _last_name;
+
+ public:
+    FIO(const std::string&, const std::string&, const std::string&);
+    FIO();
+    FIO(const FIO&);
+
+    ~FIO() = default;
+
+    void set_first_name(const std::string&);
+    void set_second_name(const std::string&);
+    void set_last_name(const std::string&);
+
+    const std::string get_first_name() const noexcept;
+    const std::string get_second_name() const noexcept;
+    const std::string get_last_name() const noexcept;
+
+ private:
+    bool check_to_correct_form(const std::string) const noexcept;
+    void convert_to_norm_form(std::string) const noexcept;
 };
 #endif  // FILM_LIBRARY_FIO_FIO_H_
