75a6dd23c1e33642babc7ae018ce0b54f23aeb9e
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-05-12 20:00:25 +0300

    added date header

--- Film_library/Date_of_birth/Date.h
+++ Film_library/Date_of_birth/Date.h
@@ -7,5 +7,24 @@ class Date {
     int day;
     int month;
     int year;
+
+ public:
+    Date();
+    Date(const int, const int, const int);
+    Date(const Date&);
+
+    ~Date() = default;
+
+    void set_day(const int&);
+    void set_month(const int&);
+    void set_year(const int&);
+
+    const int get_day() const noexcept;
+    const int get_month() const noexcept;
+    const int get_year() const noexcept;
+
+ private:
+    bool check_to_correct_date(const int&, const int&, const int&) const noexcept;
+    bool check_to_correct_day(const int& current_day, const int last_day) const noexcept;
 };
 #endif  // FILM_LIBRARY_DATE_OF_BIRTH_DATE_H_
