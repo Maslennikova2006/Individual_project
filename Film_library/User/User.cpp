408cfc60f25b7de45dad056e08a271747345bcda
Мария Масленникова <mari.maslennikova.06@mail.ru>
2025-05-24 10:02:51 +0300

    added the header and implementation of the User class

--- Film_library/User/User.cpp
+++ Film_library/User/User.cpp
@@ -1,2 +1,62 @@
 // Copyright 2025 Maslennikova Mary
+
+#include <iostream>
+
 #include "/git/Film_library/User/User.h"
+
+User::User() : _login("Username"), _password("123456pw") {}
+User::User(const std::string& login, const std::string& password) {
+	set_login(login);
+	set_password(password);
+}
+User::User(const User& other) {
+	if (&other == NULL)
+		throw std::invalid_argument("The object was not received!\n");
+	_login = other._login;
+	_password = other._password;
+}
+
+void User::set_login(const std::string& login) {
+	if (!check_to_correct_login(login))
+		throw std::invalid_argument("Incorrect login!\n");
+	_login = login;
+}
+void User::set_password(const std::string& password) {
+	if (!check_to_correct_password(password))
+		throw std::invalid_argument("Incorrect password!\n");
+	_password = password;
+}
+
+const std::string User::get_login() const noexcept {
+	return _login;
+}
+const std::string User::get_password() const noexcept {
+	return _password;
+}
+
+bool User::check_to_correct_login(const std::string& login) {
+	if (login.length() < 4 || login.length() > 20) {
+		throw std::invalid_argument
+		("The login must contain from 4 to 20 symbols!\n");
+		return false;
+	}
+
+	if (login.find(' ') != std::string::npos) {
+		throw std::invalid_argument("The login must not contain spaces!\n");
+		return false;
+	}
+	return true;
+}
+bool User::check_to_correct_password(const std::string& password) {
+	if (password.length() < 8 || password.length() > 20) {
+		throw std::invalid_argument
+		("The password must contain from 8 to 20 symbols!\n");
+		return false;
+	}
+
+	if (password.find(' ') != std::string::npos) {
+		throw std::invalid_argument("The password must not contain spaces!\n");
+		return false;
+	}
+	return true;
+}
--- Film_library/User/User.h
+++ Film_library/User/User.h
@@ -1,10 +1,29 @@
 // Copyright 2025 Maslennikova Mary
 
+#include <string>
+
 #ifndef FILM_LIBRARY_USER_USER_H_
 #define FILM_LIBRARY_USER_USER_H_
 
 class User {
-    char* _login;
-    char* _password;
+    std::string _login;
+    std::string _password;
+
+ public:
+    User();
+    User(const std::string& login, const std::string& password);
+    User(const User&);
+
+    ~User() = default;
+
+    void set_login(const std::string& login);
+    void set_password(const std::string& password);
+
+    const std::string get_login() const noexcept;
+    const std::string get_password() const noexcept;
+
+ private:
+    bool check_to_correct_login(const std::string& login);
+    bool check_to_correct_password(const std::string& password);
 };
 #endif  // FILM_LIBRARY_USER_USER_H_
